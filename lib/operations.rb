e5c658552dd6ae577a9266d1045b5a20f4c2cde9
David zhang <d.ydzdavid@yahoo.com>
2019-03-06 22:56:51 +0000

    Automatically backed up by Learn

--- lib/operations.rb
+++ lib/operations.rb
@@ -2,7 +2,7 @@ def unsafe?(speed)
   if speed < 40 || speed > 60
     return true 
   else 
-    return "safe"
+    return false
   end
 
 end
@@ -10,7 +10,7 @@ end
 
 
 def not_safe?(speed)
-  (speed > 60 || speed < 40) ? true : "safe"
+  (speed > 60 || speed < 40) ? true : false
 	
 end
 	
